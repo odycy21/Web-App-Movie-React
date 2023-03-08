4f0ebfcebc5626cc5d116fea25a1c865bd85e1c6
bryanosaldomain <102651083+bryanosaldomain@users.noreply.github.com>
2023-03-09 00:30:21 +0800

    change movie container size and removed pricing page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import '../styles/FooterStyle.css';
 
 function Footer() {
-
+    
+    const today = new Date();
+    const year = today.getFullYear();
 
     return(
        
-        <h1 className="footer-Container">  Stream Haus | 2023 All Rights Reserved</h1>
+        <p className="footer-Container">  Stream Haus | {year} All Rights Reserved</p>
 
     )
 }
