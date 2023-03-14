91b9ea04990232e616f227f141dbaafa7f25c46f
bryanosaldomain <102651083+bryanosaldomain@users.noreply.github.com>
2023-03-15 00:18:01 +0800

    fix footer 2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import NavBar from "./components/NavBar";
-import Footer from "./components/footer/index.js";
+import Footer from "./components/footer";
 
 
 function App() {
